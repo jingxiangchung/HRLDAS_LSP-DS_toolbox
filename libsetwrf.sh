76ac452e82749a4e43acddfc2f90aa50a71d0293
Chung Jing Xiang <jingxiang89@gmail.com>
2025-09-18 16:18:47 +0800

    Update libsetwrf.sh
    Add path to download the libraries source codes needed.

--- libsetwrf.sh
+++ libsetwrf.sh
@@ -28,6 +28,7 @@ export PATH=${INSTALL_DIR}/bin:${PATH}
 sudo apt-get install build-essential libevent-dev m4 csh
 
 #0.OpenMPI---------------------------------------
+#openmpi: https://www.open-mpi.org/software/ompi/v5.0/
 echo "...compiling openMPI..."
 tar -xzf ${PATH_to_SRC}/openmpi-5.0.5.tar.gz;cd openmpi-5.0.5;./configure --prefix=${INSTALL_DIR};make;make install;cd ..
 
@@ -36,7 +37,7 @@ tar -xzf ${PATH_to_SRC}/openmpi-5.0.5.tar.gz;cd openmpi-5.0.5;./configure --pref
 echo "...compiling zlib and szip..."
 tar -xzf ${PATH_to_SRC}/zlib-1.3.1.tar.gz; cd zlib-1.3.1;./configure --prefix=${INSTALL_DIR};make;make install;cd ..
 
-#szip
+#szip: https://docs.hdfgroup.org/archive/support/ftp/lib-external/szip/2.1.1/src/szip-2.1.1.tar.gz
 tar -xzf ${PATH_to_SRC}/szip-2.1.1.tar.gz; cd szip-2.1.1; ./configure --prefix=${INSTALL_DIR};make;make install;cd ..
 
 #2. PNG---------------------------------------
@@ -65,12 +66,13 @@ echo "...compiling netcdf-fortran..."
 tar -xzf ${PATH_to_SRC}/netcdf-fortran-4.6.1.tar.gz; cd netcdf-fortran-4.6.1;./configure --prefix=${INSTALL_DIR} --enable-shared;make;make install;cd ..
 
 #5. JPEG-------------------------------------
+#jpeg: https://ijg.org/files/
 echo "...compiling JPEG..."
 tar -xzf ${PATH_to_SRC}/jpegsrc.v9f.tar.gz; cd jpeg-9f; ./configure --prefix=${INSTALL_DIR};make; make install; cd ..
 
 #6. Jasper------------------------------------
+#jasper-1.900.29: https://repository.timesys.com/buildsources/j/jasper/jasper-1.900.29/jasper-1.900.29.tar.gz
 echo "...compiling jasper..."
-#jasper-1.900.29
 tar -xzf ${PATH_to_SRC}/jasper-1.900.29.tar.gz; cd jasper-1.900.29; ./configure --prefix=${INSTALL_DIR};make; make install; cd ..
 
 echo "...please run "export PATH=${INSTALL_DIR}/bin:${PATH}" when you want to compile WRF..."
